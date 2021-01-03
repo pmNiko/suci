97053325e83fb77eded2f39326943c5fc06e3b7e
pmNiko <nikolas090189@gmail.com>
2021-01-03 11:54:54 -0300

    Add structure to Till

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "@material-ui/core/styles"
-import { theme } from "./styles/themeConfig"
-import Button from "@material-ui/core/Button"
-import {Navbar} from "./components/layout/navbar"
-import KitchenPage from "./components/pages/kitchen"
-import ComandaCard from "./components/layout/comandaCard"
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./styles/themeConfig";
+import Button from "@material-ui/core/Button";
+import { Navbar } from "./components/layout/navbar";
+import KitchenPage from "./components/pages/kitchen";
+import ComandaCard from "./components/layout/comandaCard";
+import Till from "./components/Till";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar/>
-      <ComandaCard/>
+      <Navbar />
+      <Till />
     </ThemeProvider>
   );
 }

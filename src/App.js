97baf309c169038681e6c6477cd1a311cbeab5da
pmNiko <nikolas090189@gmail.com>
2021-01-03 15:41:26 -0300

    Add database items and Component Till

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./styles/themeConfig";
-import Button from "@material-ui/core/Button";
 import { Navbar } from "./components/layout/navbar";
-import KitchenPage from "./components/pages/kitchen";
-import ComandaCard from "./components/layout/comandaCard";
-import Till from "./components/Till";
+import Till from "./components/layout/Till";
 
 function App() {
   return (
---
+++ src/utils/database.js
@@ -0,0 +1,213 @@
+const items = [
+  // items categoria rapidas
+  {
+    id: 1,
+    name: "Milanesa de pollo",
+    price: 450.5,
+    category: "Rapidas",
+  },
+  {
+    id: 2,
+    name: "Milanesa de ternera",
+    price: 500.6,
+    category: "Rapidas",
+  },
+  {
+    id: 3,
+    name: "Hamburguesa",
+    price: 350.7,
+    category: "Rapidas",
+  },
+  {
+    id: 4,
+    name: "Pizza napolitana",
+    price: 780.1,
+    category: "Rapidas",
+  },
+  {
+    id: 5,
+    name: "Pizza calabresa",
+    price: 790.2,
+    category: "Rapidas",
+  },
+  {
+    id: 6,
+    name: "Empanadas de carne",
+    price: 600.2,
+    category: "Rapidas",
+  },
+
+  // items categorias platos fuertes
+  {
+    id: 7,
+    name: "Ciervo a la cazadora",
+    price: 560.45,
+    category: "Platos fuertes",
+  },
+  {
+    id: 8,
+    name: "Trucha al limon",
+    price: 780.45,
+    category: "Platos fuertes",
+  },
+  {
+    id: 9,
+    name: "Ñoquis a la boloñeza",
+    price: 460.9,
+    category: "Platos fuertes",
+  },
+  {
+    id: 10,
+    name: "Ñoquis a la crema",
+    price: 780.3,
+    category: "Platos fuertes",
+  },
+  {
+    id: 11,
+    name: "Ravioles de verdura con filleto",
+    price: 800.3,
+    category: "Platos fuertes",
+  },
+  {
+    id: 12,
+    name: "Ravioles de verdura con crema",
+    price: 800.3,
+    category: "Platos fuertes",
+  },
+
+  // Items categoria ensaladas
+  {
+    id: 13,
+    name: "Ensalada mixta",
+    price: 246.85,
+    category: "Ensaladas",
+  },
+  {
+    id: 14,
+    name: "Ensalada primavera",
+    price: 341.97,
+    category: "Ensaladas",
+  },
+  {
+    id: 15,
+    name: "Ensalada de trucha",
+    price: 350.0,
+    category: "Ensaladas",
+  },
+  {
+    id: 16,
+    name: "Ensalada César",
+    price: 320.47,
+    category: "Ensaladas",
+  },
+  {
+    id: 17,
+    name: "Ensalada mixta completa",
+    price: 462.8,
+    category: "Ensaladas",
+  },
+
+  // Items categoria postres
+  {
+    id: 18,
+    name: "Lemon pie",
+    price: 124.45,
+    category: "Postres",
+  },
+  {
+    id: 19,
+    name: "Torta selva negra",
+    price: 264.87,
+    category: "Postres",
+  },
+  {
+    id: 20,
+    name: "Flan",
+    price: 200.0,
+    category: "Postres",
+  },
+  {
+    id: 21,
+    name: "Waffle",
+    price: 150.5,
+    category: "Postres",
+  },
+  {
+    id: 22,
+    name: "Helado",
+    price: 260.42,
+    category: "Postres",
+  },
+  {
+    id: 23,
+    name: "Ensalada de fruta",
+    price: 148.9,
+    category: "Postres",
+  },
+
+  // Items categoria Refresco
+  {
+    id: 24,
+    name: "Gaseosa",
+    price: 128.9,
+    category: "Refresco",
+  },
+  {
+    id: 25,
+    name: "Jugo natural",
+    price: 110.9,
+    category: "Refresco",
+  },
+  {
+    id: 26,
+    name: "Cerveza",
+    price: 200.9,
+    category: "Refresco",
+  },
+  {
+    id: 27,
+    name: "Vino",
+    price: 150.9,
+    category: "Refresco",
+  },
+
+  // Items categoria Bebibles
+  {
+    id: 28,
+    name: "Te",
+    price: 80.5,
+    category: "Bebibles",
+  },
+  {
+    id: 29,
+    name: "Capuccino",
+    price: 90.9,
+    category: "Bebibles",
+  },
+  {
+    id: 30,
+    name: "Chocolate",
+    price: 70.2,
+    category: "Bebibles",
+  },
+  {
+    id: 31,
+    name: "Cafe a la crema",
+    price: 100.1,
+    category: "Bebibles",
+  },
+  {
+    id: 32,
+    name: "Cafe expresso",
+    price: 120.9,
+    category: "Bebibles",
+  },
+  {
+    id: 33,
+    name: "Cafe cortado",
+    price: 125.8,
+    category: "Bebibles",
+  },
+];
+
+export default items;
